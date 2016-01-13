8af4753c8f39121819d32c6c270af1ae55bee4ef
Harry Ng <harryworld@gmail.com>
2016-01-13 17:42:21 +0800

    Add NSCollectionView to storyboard

--- NSCollectionView-Header-Footer/ViewController.swift
+++ NSCollectionView-Header-Footer/ViewController.swift
@@ -10,6 +10,8 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var collectionView: NSCollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
