9e2072a20ffd553e3ada10f66893741594f3b0b5
Michael Critz <michael.critz@sony.com>
2018-05-01 08:25:14 -0700

    updated test code to 4.1

--- NSCollectionView-Header-FooterTests/NSCollectionView_Header_FooterTests.swift
+++ NSCollectionView-Header-FooterTests/NSCollectionView_Header_FooterTests.swift
@@ -28,9 +28,8 @@ class NSCollectionView_Header_FooterTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+        self.measure({
+        })
     }
     
 }
